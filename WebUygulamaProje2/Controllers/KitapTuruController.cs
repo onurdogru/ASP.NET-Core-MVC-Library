d11c115559587c07aa903e7a35e3e47da33387bc
Onur <onurdogruiletisim@gmail.com>
2025-05-12 23:06:52 +0300

    dependency

--- WebUygulamaProje2/Controllers/KitapTuruController.cs
+++ WebUygulamaProje2/Controllers/KitapTuruController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebUygulamaProje2.Models;
+using WebUygulamaProje2.Utility;
 
 namespace WebUygulamaProje2.Controllers
 {
     public class KitapTuruController : Controller
     {
+        //12.05.2025
+        private readonly UygulamaDbContext _uygulamaDbContext; //=new uygulamaDB() konsolda nesne yapma mantığı gibi
+
+        public KitapTuruController(UygulamaDbContext context) 
+        {
+            _uygulamaDbContext = context;
+        }
+
+
+
+
         public IActionResult Index()
         {
+            List<KitapTuru> objKitapTuruList = _uygulamaDbContext.KitapTurleri.ToList(); //veri tabanına gidip kitap türleri çek ve objKitapTuruList'esine alacak.
             return View();
         }
     }
